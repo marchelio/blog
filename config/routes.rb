88dab4ad8a7a30fac027f709719718f13eefa021
marchelio <marchelio479@gmail.com>
2014-08-07 19:23:01 +0300

    new com

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   match '/home', to: 'static_pages#home', via: 'get'
 
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
@@ -15,6 +18,7 @@ Rails.application.routes.draw do
   end
   #users by 2app
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'welcome/index'
 
